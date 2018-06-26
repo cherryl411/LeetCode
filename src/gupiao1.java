5d10342b9900d7fe81f412218f6a66ec9271c904
lnc <lnc8023@163.com>
2018-06-26 22:32:38 +0800

    1.股票第二题——贪心算法

--- src/gupiao1.java
+++ src/gupiao1.java
@@ -4,16 +4,9 @@ public class gupiao1 {
         if (prices == null || prices.length < 1){
             return result;
         }
-        int[] profit = new int[prices.length];
-        int min = 0;
-        int max = prices[0];
-        profit[0] = prices[0];
         for (int i = 1; i < prices.length; i++){
-            profit[i] = Math.max(profit[i-1], prices[i] - min);
-            min = Math.min(min, prices[i]);
-            max = Math.max(profit[i], max);
+            if (prices[i] > prices[i-1]) result += prices[i] - prices[i-1];
         }
-        result = max;
         return result;
     }
 }
